def82c922e9342b7492c5ac994478a1e5b2b069a
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-05-18 17:36:30 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *
  * Return:always 0
  */
-int main (void)
+int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle/n");
 
 	return (0);
 }
