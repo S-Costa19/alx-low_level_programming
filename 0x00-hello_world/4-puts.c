0657cfdc002842aa6207f4624d510b8d838f1589
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-05-18 17:22:10 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - put Programming is like building a multilingual puzzle
+ * followed by new line
+ *
+ * Return:always 0
+ */
+int main (void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
