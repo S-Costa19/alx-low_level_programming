bfc687cec0d558da4fd8da97f2191343d00ad70c
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-05-18 19:11:40 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
 /**
- * main - print exactly and that piece of art is useful" - Dora Korpar,
- * 2015-10-19,  followed by new line
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return:always 1
+ * Return: Always 1.
  */
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
