f4ca02e62fd3802bc474b78eda703cd6bb70e5ae
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-05-18 18:33:03 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - print exactly and that piece of art is useful" - Dora Korpar,
  * 2015-10-19,  followed by new line
@@ -7,9 +8,8 @@
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
+		write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of a char : %zu bytes(S)\n", sizeof(int));
-	printf("Size of a int : %zu bytes(S)\n", sizeof(int));
-	printf("Size of a long int : %zu bytes(S)\n", sizeof(long int));
-	printf("Size of a long long int : %zu bytes(S)\n", sizeof(long long int));
-	printf("Size of a float : %zu bytes(S)\n, sizeof(float));
+	printf("Size of a char : %lu bytes(S)\n", sizeof(int));
+	printf("Size of a int : %lu bytes(S)\n", sizeof(int));
+	printf("Size of a long int : %lu bytes(S)\n", sizeof(long int));
+	printf("Size of a long long int : %lu bytes(S)\n", sizeof(long long int));
+	printf("Size of a float : %lu bytes(S)\n, sizeof(float));
 
 	return (0);
 }
