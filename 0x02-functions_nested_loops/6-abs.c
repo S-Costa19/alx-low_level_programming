f5d1ddeb03c51c1acf91bf9a9339713d08bfcc3e
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-05-24 06:47:48 -0700

    done

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,17 +8,31 @@
  * Return: 0
  */
 int _abs(int n)
+
 {
+
 if (n > 0)
+
 {
 
-return (1);
+return (n);
+
 }
+
 else if (n < 0)
+
 {
 
-return (-1);
+return (-n);
+
 }
 
-return (0);
+else
+
+{
+
+return (n);
+
+}
+
 }
