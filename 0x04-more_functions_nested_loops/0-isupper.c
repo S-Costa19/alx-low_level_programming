6c0f5d52130d110380eda26657c56e6abb89f3c4
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-05-25 06:41:13 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /*
- * _isalpha.c - entry point
+ * _isalpha - entry point
  * @c: the the value its recieved
  * description: check if alphabet is lower or upper
- * Return: 1 if true. 0 is false
+ * Return: 1 when the character is upper. 0 otherwise
  */
 int _isupper(int c)
 
