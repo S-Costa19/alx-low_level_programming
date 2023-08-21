cd2802e7cef10ac62f690ca927273983c647a332
S-Costa19cd <shmasuddeenmuazu11@gmail.com>
2023-08-21 15:03:21 -0700

    done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Mbykz01/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
+wget -P /tmp/ https://raw.github.com/S-Costa19/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
